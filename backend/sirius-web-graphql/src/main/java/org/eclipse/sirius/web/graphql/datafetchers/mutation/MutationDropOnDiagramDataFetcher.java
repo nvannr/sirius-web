e17cd96d3cd00a906280d398199d3322acfe1b88
Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>
2021-12-20 11:56:53 +0100

    [132] Add support for drag and drop from the explorer to a diagram
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/132
    Signed-off-by: Stéphane Bégaudeau <stephane.begaudeau@obeo.fr>

---
+++ backend/sirius-web-graphql/src/main/java/org/eclipse/sirius/web/graphql/datafetchers/mutation/MutationDropOnDiagramDataFetcher.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.graphql.datafetchers.mutation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.sirius.web.annotations.spring.graphql.MutationDataFetcher;
+import org.eclipse.sirius.web.core.api.ErrorPayload;
+import org.eclipse.sirius.web.core.api.IPayload;
+import org.eclipse.sirius.web.graphql.messages.IGraphQLMessageService;
+import org.eclipse.sirius.web.graphql.schema.MutationTypeProvider;
+import org.eclipse.sirius.web.spring.collaborative.api.IEditingContextEventProcessorRegistry;
+import org.eclipse.sirius.web.spring.collaborative.diagrams.dto.DropOnDiagramInput;
+import org.eclipse.sirius.web.spring.graphql.api.IDataFetcherWithFieldCoordinates;
+
+import graphql.schema.DataFetchingEnvironment;
+
+/**
+ * The data fetcher used to perform a drop on a diagram.
+ *
+ * @author sbegaudeau
+ */
+@MutationDataFetcher(type = MutationTypeProvider.TYPE, field = MutationDropOnDiagramDataFetcher.DROP_ON_DIAGRAM_FIELD)
+public class MutationDropOnDiagramDataFetcher implements IDataFetcherWithFieldCoordinates<CompletableFuture<IPayload>> {
+
+    public static final String DROP_ON_DIAGRAM_FIELD = "dropOnDiagram"; //$NON-NLS-1$
+
+    private final ObjectMapper objectMapper;
+
+    private final IEditingContextEventProcessorRegistry editingContextEventProcessorRegistry;
+
+    private final IGraphQLMessageService messageService;
+
+    public MutationDropOnDiagramDataFetcher(ObjectMapper objectMapper, IEditingContextEventProcessorRegistry editingContextEventProcessorRegistry, IGraphQLMessageService messageService) {
+        this.objectMapper = Objects.requireNonNull(objectMapper);
+        this.editingContextEventProcessorRegistry = Objects.requireNonNull(editingContextEventProcessorRegistry);
+        this.messageService = Objects.requireNonNull(messageService);
+    }
+
+    @Override
+    public CompletableFuture<IPayload> get(DataFetchingEnvironment environment) throws Exception {
+        Object argument = environment.getArgument(MutationTypeProvider.INPUT_ARGUMENT);
+        var input = this.objectMapper.convertValue(argument, DropOnDiagramInput.class);
+
+     // @formatter:off
+     return this.editingContextEventProcessorRegistry.dispatchEvent(input.getEditingContextId(), input)
+             .defaultIfEmpty(new ErrorPayload(input.getId(), this.messageService.unexpectedError()))
+             .toFuture();
+     // @formatter:on
+    }
+
+}
