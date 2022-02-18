7e81dc8cf4aed08cdf570b8c44f3f5b619805e85
Guillaume Coutable <guillaume.coutable@obeo.fr>
2022-02-18 14:23:34 +0100

    [997] Add support for edge routing points edition
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/997
    Signed-off-by: Guillaume Coutable <guillaume.coutable@obeo.fr>

---
+++ backend/sirius-web-graphql/src/main/java/org/eclipse/sirius/web/graphql/datafetchers/mutation/MutationUpdateEdgeRoutingPointsDataFetcher.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.graphql.datafetchers.mutation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.sirius.components.annotations.spring.graphql.MutationDataFetcher;
+import org.eclipse.sirius.components.collaborative.api.IEditingContextEventProcessorRegistry;
+import org.eclipse.sirius.components.collaborative.diagrams.dto.UpdateEdgeRoutingPointsInput;
+import org.eclipse.sirius.components.core.api.ErrorPayload;
+import org.eclipse.sirius.components.core.api.IPayload;
+import org.eclipse.sirius.components.graphql.api.IDataFetcherWithFieldCoordinates;
+import org.eclipse.sirius.web.graphql.messages.IGraphQLMessageService;
+import org.eclipse.sirius.web.graphql.schema.MutationTypeProvider;
+
+import graphql.schema.DataFetchingEnvironment;
+
+/**
+ * The data fetcher used to update routing points of an edge.
+ * <p>
+ * It will be used to handle the following GraphQL field:
+ * </p>
+ *
+ * <pre>
+ * type Mutation {
+ *   updateEdgeRoutingPoints(input: UpdateEdgeRoutingPointsInput!): UpdateEdgeRoutingPointsPayload!
+ * }
+ * </pre>
+ *
+ * @author gcoutable
+ */
+@MutationDataFetcher(type = MutationTypeProvider.TYPE, field = MutationUpdateEdgeRoutingPointsDataFetcher.UPDATE_EDGE_ROUTING_POINTS_FIELD)
+public class MutationUpdateEdgeRoutingPointsDataFetcher implements IDataFetcherWithFieldCoordinates<CompletableFuture<IPayload>> {
+    public static final String UPDATE_EDGE_ROUTING_POINTS_FIELD = "updateEdgeRoutingPoints"; //$NON-NLS-1$
+
+    private final ObjectMapper objectMapper;
+
+    private final IEditingContextEventProcessorRegistry editingContextEventProcessorRegistry;
+
+    private final IGraphQLMessageService messageService;
+
+    public MutationUpdateEdgeRoutingPointsDataFetcher(ObjectMapper objectMapper, IEditingContextEventProcessorRegistry editingContextEventProcessorRegistry, IGraphQLMessageService messageService) {
+        this.objectMapper = Objects.requireNonNull(objectMapper);
+        this.editingContextEventProcessorRegistry = Objects.requireNonNull(editingContextEventProcessorRegistry);
+        this.messageService = Objects.requireNonNull(messageService);
+    }
+
+    @Override
+    public CompletableFuture<IPayload> get(DataFetchingEnvironment environment) throws Exception {
+        Object argument = environment.getArgument(MutationTypeProvider.INPUT_ARGUMENT);
+        var input = this.objectMapper.convertValue(argument, UpdateEdgeRoutingPointsInput.class);
+
+        // @formatter:off
+        return this.editingContextEventProcessorRegistry.dispatchEvent(input.getEditingContextId(), input)
+                .defaultIfEmpty(new ErrorPayload(input.getId(), this.messageService.unexpectedError()))
+                .toFuture();
+        // @formatter:on
+    }
+}
