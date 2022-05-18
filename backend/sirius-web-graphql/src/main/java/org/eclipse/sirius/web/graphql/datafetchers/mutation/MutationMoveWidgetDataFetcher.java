93968b9b1b9e2c3fefa21fafbeb0e8ccd845db53
Axel RICHARD <axel.richard@obeo.fr>
2022-05-18 14:17:20 +0200

    [1169] Handle move widget mutation in FormDescriptionEditor
    Bug: https://github.com/eclipse-sirius/sirius-components/issues/1169
    Signed-off-by: Axel RICHARD <axel.richard@obeo.fr>

---
+++ backend/sirius-web-graphql/src/main/java/org/eclipse/sirius/web/graphql/datafetchers/mutation/MutationMoveWidgetDataFetcher.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.sirius.web.graphql.datafetchers.mutation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.sirius.components.annotations.spring.graphql.MutationDataFetcher;
+import org.eclipse.sirius.components.collaborative.api.IEditingContextEventProcessorRegistry;
+import org.eclipse.sirius.components.collaborative.formdescriptioneditors.dto.MoveWidgetInput;
+import org.eclipse.sirius.components.core.api.ErrorPayload;
+import org.eclipse.sirius.components.core.api.IPayload;
+import org.eclipse.sirius.components.graphql.api.IDataFetcherWithFieldCoordinates;
+import org.eclipse.sirius.web.graphql.messages.IGraphQLMessageService;
+import org.eclipse.sirius.web.graphql.schema.MutationTypeProvider;
+
+import graphql.schema.DataFetchingEnvironment;
+
+/**
+ * The data fetcher used to move a widget in a Form Description Editor
+ * <p>
+ * It will be used to handle the following GraphQL field:
+ * </p>
+ *
+ * <pre>
+ * type Mutation {
+ *   moveWidget(input: MoveWidgetInput!): MoveWidgetPayload!
+ * }
+ * </pre>
+ *
+ * @author arichard
+ *
+ */
+@MutationDataFetcher(type = MutationTypeProvider.TYPE, field = MutationMoveWidgetDataFetcher.ADD_WIDGET_FIELD)
+public class MutationMoveWidgetDataFetcher implements IDataFetcherWithFieldCoordinates<CompletableFuture<IPayload>> {
+    public static final String ADD_WIDGET_FIELD = "moveWidget"; //$NON-NLS-1$
+
+    private final ObjectMapper objectMapper;
+
+    private final IEditingContextEventProcessorRegistry editingContextEventProcessorRegistry;
+
+    private final IGraphQLMessageService messageService;
+
+    public MutationMoveWidgetDataFetcher(ObjectMapper objectMapper, IEditingContextEventProcessorRegistry editingContextEventProcessorRegistry, IGraphQLMessageService messageService) {
+        this.objectMapper = Objects.requireNonNull(objectMapper);
+        this.editingContextEventProcessorRegistry = Objects.requireNonNull(editingContextEventProcessorRegistry);
+        this.messageService = Objects.requireNonNull(messageService);
+    }
+
+    @Override
+    public CompletableFuture<IPayload> get(DataFetchingEnvironment environment) throws Exception {
+        Object argument = environment.getArgument(MutationTypeProvider.INPUT_ARGUMENT);
+        var input = this.objectMapper.convertValue(argument, MoveWidgetInput.class);
+
+        // @formatter:off
+        return this.editingContextEventProcessorRegistry.dispatchEvent(input.getEditingContextId(), input)
+                .defaultIfEmpty(new ErrorPayload(input.getId(), this.messageService.unexpectedError()))
+                .toFuture();
+        // @formatter:on
+    }
+}
